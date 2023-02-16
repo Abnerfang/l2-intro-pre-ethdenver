c05c5ac8d2469a8c7ae8601cf0c66d022f685541
abner <maginacn@live.cn>
2023-02-16 12:38:37 +0800

    bitfang deployed a zk-contract on testnet
    Signed-off-by: abner <maginacn@live.cn>

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.0;
 contract Greeter {
     string private greeting;
 
+    string private bitfanggreetings;
+
     constructor(string memory _greeting) {
         greeting = _greeting;
+        bitfanggreetings = "bitfang says hello to zksync!";
     }
 
     function greet() public view returns (string memory) {
@@ -14,5 +17,9 @@ contract Greeter {
 
     function setGreeting(string memory _greeting) public {
         greeting = _greeting;
+    }
+
+    function bitfanggreet() public view returns (string memory) {
+        return bitfanggreetings;
     }
 }
--- contracts/zkToken.sol
+++ contracts/zkToken.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract zkToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable {
-    constructor() ERC20("zkToken", "ZZZK") {
+    constructor() ERC20("bitfangzkToken", "BITFANGZK") {
         _mint(msg.sender, 333333333 * 10 ** decimals());
     }
 
